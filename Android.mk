a39530cb028becb3396cd3097aea9bdcce2c956e
El Dainosor <eldainosor@gmail.com>
2020-04-22 02:06:21 +0000

    add a dummy thicc broadcastreceiver
    Change-Id: Ibce4edc3f164da8defbffbb3a74bb1793c7e6a94

--- Android.mk
+++ Android.mk
@@ -17,6 +17,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
+
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 ifeq ($(BOOTLEGGERS_NOTCHED),true)
